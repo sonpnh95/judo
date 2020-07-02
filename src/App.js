716397e8e70b668fe0f1ac4f61b288f927b0479b
sonpham <sonpnh95@gmail.com>
2020-07-02 17:19:04 +0700

    update next step

--- src/App.js
+++ src/App.js
@@ -1,120 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import firebase, { auth, provider }  from './firebase.js';
+
+const loading = (
+	<div className="pt-3 text-center">
+		<div className="sk-spinner sk-spinner-pulse"></div>
+	</div>
+)
+
+// Containers
+const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'));
+const Register = React.lazy(() => import('./views/pages/register/Register'));
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentItem: '',
-      username: '',
-      items: [],
-      user: null
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.login = this.login.bind(this); 
-    this.logout = this.logout.bind(this); 
-  }
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      title: this.state.currentItem,
-      user: this.state.username
-    }
-    itemsRef.push(item);
-    this.setState({
-      currentItem: '',
-      username: ''
-    });
-  }
-  componentDidMount() {
-    const itemsRef = firebase.database().ref('items');
-
-    itemsRef.on('value', (snapshot) => {
-      let items = snapshot.val();
-      let newState = [];
-      for (let item in items) {
-        newState.push({
-          id: item,
-          title: items[item].title,
-          user: items[item].user
-        });
-      }
-      this.setState({
-        items: newState
-      });
-    });
-  }
-
-  removeItem(itemId) {
-    const itemRef = firebase.database().ref(`/items/${itemId}`);
-    itemRef.remove();
-  }
-
-  login() {
-    auth.signInWithPopup(provider) 
-      .then((result) => {
-        const user = result.user;
-        this.setState({
-          user
-        });
-      });
-  }
-
-  logout() {
-    // we will add the code for this in a moment, but need to add the method now or the bind will throw an error
-  }
-
-  render() {
-    return (
-      <div className='app'>
-        <header>
-            <div className="wrapper">
-              <h1>Fun Food Friends</h1>
-              <i className="fa fa-shopping-basket" aria-hidden="true"></i>            
-              {this.state.user ?
-                <button onClick={this.logout}>Log Out</button>                
-                :
-                <button onClick={this.login}>Log In</button>              
-              }   
-            </div>
-        </header>
-        <div className='container'>
-          <section className='add-item'>
-                <form onSubmit={this.handleSubmit}>
-                  <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                  <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                  <button>Add Item</button>
-                </form>
-          </section>
-          <section className='display-item'>
-              <div className="wrapper">
-                <ul>
-                  {this.state.items.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>brought by: <strong>{item.user}</strong>
-                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                        </p>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-          </section>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<HashRouter>
+				<React.Suspense fallback={loading}>
+					<Switch>
+						<Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
+						<Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
+						<Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
+						<Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
+						<Route path="/" name="Home" render={props => <TheLayout {...props} />} />
+					</Switch>
+				</React.Suspense>
+			</HashRouter>
+		);
+	}
 }
 
 export default App;
